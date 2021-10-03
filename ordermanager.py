8afb76eaf2f3123b4bf646f9a1ca26a7ba42b90e
Phil Mann <pmann1053@gmail.com>
2021-10-02 23:49:01 -0400

    skeleton code in order mangager

--- ordermanager.py
+++ ordermanager.py
@@ -3,16 +3,43 @@
 # moves stoploss to take profits
 # https://tda-api.readthedocs.io/en/latest/client.html#orders
 from signaler import Signals
+from enum import Enum
+
+
+class PositionState(Enum):
+    OPEN = auto()
+    TRAIL_STOP = auto()
+
+
+class OrderManagerConfig:
+    def __init__(self,):
+        pass
+
 
 class Position:
     def __init__(self, contract):
-        self.contract = contract
+        self.contract = contract # contract symbol
         self.netpos = 0
         self.associated_orders = {} # id:status
+
+        se;f.state = None # PositionState
         self.stop = None
+        self.takeprofit = []
+
+    # possibly move these into order manager
+    def open():
+        pass
+
+    def close():
+        pass
+
+    def increase():
+        pass
+
 
 class OrderManager:
-    def __init__(self):
+    def __init__(self, config,):
+        self.config = config # class OrderManagerConfig
         self.currentpositions = {} # symbol:Position
 
     def update(self, symbol, signal, newprice):
@@ -22,3 +49,19 @@ class OrderManager:
     so update should be Signals.something
     """
         pass
+
+    def getContractFromChain():
+    """
+    returns an appropriate options contract symbol
+    """
+        pass
+
+    def open():
+        pass
+
+    def close():
+        pass
+
+    def increase():
+        pass
+
