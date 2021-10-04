e461180b96bb7fa869dab99e30d6443696eb8891
Phil Mann <pmann1053@gmail.com>
2021-10-04 01:04:28 -0400

    options chain flattening

--- botutils.py
+++ botutils.py
@@ -9,6 +9,9 @@ import httpx
 
 
 async def gethistory(client, symbol):
+    """
+    returns today's minute-by-minute OHCLV history
+    """
     resp = client.get_price_history(
         symbol,
         period_type=Client.PriceHistory.PeriodType.DAY,
@@ -29,6 +32,11 @@ async def gethistory(client, symbol):
 async def getOptionChain(
     client, symbol, strike_count, dte,
 ):
+    """
+    returns the option chain of the requested symbol.
+    returned as-is it's nested in a way that can
+    be inconvenient. see flatten()
+    """
     resp = client.get_option_chain(
         symbol,
         strike_count=strike_count,
@@ -42,3 +50,32 @@ async def getOptionChain(
     chain = resp.json()
 
     return chain
+
+
+async def flatten(chain):
+    """
+    take input from getOptionChain()
+    and flatten so that each contract is on its own.
+    can then be sorted easily with comprehensions for example
+
+    would like to turn this into a comprehension rather than nested loops
+    """
+
+    flattened = []
+    for key in ['callExpDateMap', 'putExpDateMap']:
+        for date in chain[key]:
+            for strike in chain[key][date]:
+                for contract in chain[key][date][strike]:
+                    flattened.append(contract)
+
+    return flattened
+
+async def getFlattenedChain(
+    client, symbol, strike_count, dte,
+):
+    """
+    combines the flatten and getOptionChain functions
+    """
+    chain = await getOptionChain(client, symbol, strike_count, dte,)
+    flattened = await flatten(chain)
+    return flattened
