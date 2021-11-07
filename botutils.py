c3e9120785c7e038d8aca087751cbbf4ba0bd1c7
Phil Mann <pmann1053@gmail.com>
2021-11-06 22:39:06 -0400

    pylint

--- botutils.py
+++ botutils.py
@@ -1,20 +1,16 @@
-# utility funcs and classes
+"""Utility functions and classes used in philbot."""
 
 from statistics import stdev
-import asyncio
 import datetime
-import json
 import re
 import time
+from requests import HTTPError
 
 from tda.client import Client
-import httpx
 
 
-def gethistory(client, symbol):
-    """
-    returns today's minute-by-minute OHCLV history
-    """
+def get_history(client, symbol):
+    """Returns today's minute-by-minute OHCLV history for the requested symbol."""
     while True:
         try:
             resp = client.get_price_history(
@@ -23,46 +19,45 @@ def gethistory(client, symbol):
                 period=Client.PriceHistory.Period.ONE_DAY,
                 frequency_type=Client.PriceHistory.FrequencyType.MINUTE,
                 frequency=Client.PriceHistory.Frequency.EVERY_MINUTE,
+                # end_datetime defaults to yesterday, necessitating the following
                 end_datetime=datetime.datetime.today() + datetime.timedelta(days=1),
             )
             resp.raise_for_status()
             break
-        except Exception as e:
-            print(e)
+        except HTTPError as http_error:
+            print(http_error)
             time.sleep(0.5)
 
     history = resp.json()
     return history["candles"]
 
 
-def getStdDev(candles, period, values="close"):
+def get_std_dev(candles, period, values="close"):
     """
-    takes input of gethistory.
-    uses close vals by default
-    returns standard deviation of the period
+    Returns standard deviation of the period.
+    Takes input of get_history.
+    Uses close vals by default.
     """
     return stdev([candle[values] for candle in candles[-period:]])
 
 
-def getStdDevForSymbol(client, symbol, period):
-    """
-    convenience func to combin getStdDev and gethistory
-    """
-    history = gethistory(client, symbol)
-    standard_deviation = getStdDev(history, period)
+def get_std_dev_for_symbol(client, symbol, period):
+    """Convenience func to combine get_std_dev and get_history."""
+    history = get_history(client, symbol)
+    standard_deviation = get_std_dev(history, period)
     return standard_deviation
 
 
-def getOptionChain(
+def get_option_chain(
     client,
     symbol,
     strike_count,
     dte,
 ):
     """
-    returns the option chain of the requested symbol.
-    returned as-is it's nested in a way that can
-    be inconvenient. see flatten()
+    Returns the option chain of the requested symbol.
+    Returned as-is. It's nested in a way that can be inconvenient.
+    See flatten() for extraction of the contracts.
     """
     while True:
         try:
@@ -74,22 +69,19 @@ def getOptionChain(
             )
             resp.raise_for_status()
             break
-        except Exception as e:
-            print(e)
+        except HTTPError as http_error:
+            print(http_error)
             time.sleep(0.5)
 
     chain = resp.json()
-
     return chain
 
 
 def flatten(chain):
     """
-    take input from getOptionChain()
+    Take input from get_option_chain()
     and flatten so that each contract is on its own.
-    can then be sorted easily with comprehensions for example
-
-    would like to turn this into a comprehension rather than nested loops
+    Can then be sorted easily with comprehensions for example.
     """
 
     flattened = []
@@ -102,16 +94,14 @@ def flatten(chain):
     return flattened
 
 
-def getFlattenedChain(
+def get_flattened_chain(
     client,
     symbol,
     strike_count,
     dte,
 ):
-    """
-    combines the flatten and getOptionChain functions
-    """
-    chain = getOptionChain(
+    """Combines the flatten and get_option_chain functions."""
+    chain = get_option_chain(
         client,
         symbol,
         strike_count,
@@ -123,15 +113,14 @@ def getFlattenedChain(
 
 class AccountActivityXMLParse:
     """
-    for parsing the xml data returned by the account activity stream.
-    gets data associated with the init parameter tags
+    For parsing the xml data returned by the account activity stream.
+    Gets data associated with the init parameter tags.
     """
 
     def __init__(self, tags=None):
         """
-        tags = None for a default list of tags. the default list should
+        tags = None for a default list of tags. The default list should
         be pretty exhaustive of relevant ones.
-        ignore any tags the message is missing.
         """
         self.tags = tags or [
             "OrderKey",
@@ -148,16 +137,20 @@ class AccountActivityXMLParse:
             "LastUpdated",
         ]
 
+
+    def update_tags(self, new_tags):
+        """Update self.tags by appending new tags."""
+        self.tags.extend(new_tags)
+
+
     def parse(self, xmlstring):
         """
-        take a string of the xml returned from an account
-        activity stream message and return a dictionary
-        of data relevant to the tags in self.tags.
-        return format {tag:data} both str
+        Take a string of the xml returned from an account activity stream message
+        and return a dictionary of data relevant to the tags in self.tags.
+        return format: {tag:data} both strings.
 
-        naively parses the xml by splitting on < and >
-        then associating any found tags with the data
-        that follows it in the xml string.
+        Naively parses the xml by splitting on < and >
+        then associating any found tags with the data that follows said tag in the xml string.
         """
         relevant_account_data = {}  # tag:data
         splitxml = re.split("[<>]", xmlstring)
