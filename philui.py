9e5acc859304385c115e1a51ef04fc77a6a5df99
Phil Mann <pmann1053@gmail.com>
2021-11-12 19:32:13 -0500

    skeleton code

--- philui.py
+++ philui.py
@@ -24,24 +24,30 @@ class PhilbotUI:
 
 def interface_clear(term):
     """
-    Clears the terminal and sets location to home (0,0) -
+    Clears the terminal and sets location to home (0,0) - (top left)
     as suggested at:
     https://blessed.readthedocs.io/en/stable/location.html
     """
     print(term.home + term.clear, end='')
 
-if __name__ == "__main__":
-    term = Terminal()
+def main():
+    pass
 
-    interface_clear(term)
-    print(term.blue('this is the top'))
-    print(term.move_y(term.height // 2) + term.black_on_darkkhaki(term.center('this is the middle')))
-    print(term.home + term.move_down(term.height) + 'this is the bottom', end='')
+if __name__ == '__main__':
+    main()
 
-    with term.cbreak(), term.hidden_cursor():
-        while True:
-            inp = term.inkey()
-            print(
-                term.home + term.move_down(term.height-3) + term.clear_eol +
-                'You pressed ' + term.bold(repr(inp))
-                )
+# if __name__ == "__main__":
+#    term = Terminal()
+#
+#    interface_clear(term)
+#    print(term.blue('this is the top'))
+#    print(term.move_y(term.height // 2) + term.black_on_darkkhaki(term.center('this is the middle')))
+#    print(term.home + term.move_down(term.height) + 'this is the bottom', end='')
+#
+#    with term.cbreak(), term.hidden_cursor():
+#        while True:
+#            inp = term.inkey()
+#            print(
+#                term.home + term.move_down(term.height-3) + term.clear_eol +
+#                'You pressed ' + term.bold(repr(inp))
+#                )
